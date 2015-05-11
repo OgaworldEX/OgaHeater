f23d5176b7f21877b1ebdabec449080790e85441
ogaworldex <ogaogaoga9@gmail.com>
2015-05-11 21:16:29 +0900

    tab to space

--- src/com/ogaworldexplosion/ogaheater/ui/MainFrame.java
+++ src/com/ogaworldexplosion/ogaheater/ui/MainFrame.java
@@ -12,63 +12,63 @@ import java.awt.Toolkit;
 
 public class MainFrame extends JFrame {
 
-	private static final long serialVersionUID = -8424734531682214227L;
-	private static final String AppName = "OgaHeater";
-	private static final String Version = "1.0";
-	private JPanel contentPane;
-	
-	private int maxCoreCount = Runtime.getRuntime().availableProcessors();
+    private static final long serialVersionUID = -8424734531682214227L;
+    private static final String AppName = "OgaHeater";
+    private static final String Version = "1.0";
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private int maxCoreCount = Runtime.getRuntime().availableProcessors();
 
-	/**
-	 * Create the frame.
-	 */
-	public MainFrame() {
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    MainFrame frame = new MainFrame();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-		try{
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-			SwingUtilities.updateComponentTreeUI(this);
-		}catch(Exception e){
-		  //e.printStackTrace();
-		}
-		
-		setIconImage(Toolkit.getDefaultToolkit().getImage(MainFrame.class.getResource("/com/ogaworldexplosion/ogaheater/images/img_fire_on.png")));
-		setTitle(AppName + Version);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(200, 200, 100, 100);
-		contentPane = new JPanel(new FlowLayout());
-		setContentPane(contentPane);
-				
-		int tmpCoreCount = Runtime.getRuntime().availableProcessors();
-		if(tmpCoreCount == 1){
-			maxCoreCount = tmpCoreCount;
-		}else{
-			maxCoreCount = tmpCoreCount-1;
-		}
-		
-		for(int i=0;i<maxCoreCount;i++){
-			contentPane.add(new FireButton());
-		}
+    /**
+     * Create the frame.
+     */
+    public MainFrame() {
 
-		FireButton sizeTmp = new FireButton();
-		int fireButtonHeight = (int)Math.floor(sizeTmp.getPreferredSize().getHeight());
-		int fireButtonWidth = (int)Math.floor(sizeTmp.getPreferredSize().getWidth());
-		
-		setSize(35+fireButtonWidth*this.maxCoreCount,45+fireButtonHeight);
-	}
+        try{
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            SwingUtilities.updateComponentTreeUI(this);
+        }catch(Exception e){
+          //e.printStackTrace();
+        }
+
+        setIconImage(Toolkit.getDefaultToolkit().getImage(MainFrame.class.getResource("/com/ogaworldexplosion/ogaheater/images/img_fire_on.png")));
+        setTitle(AppName + Version);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(200, 200, 100, 100);
+        contentPane = new JPanel(new FlowLayout());
+        setContentPane(contentPane);
+
+        int tmpCoreCount = Runtime.getRuntime().availableProcessors();
+        if(tmpCoreCount == 1){
+            maxCoreCount = tmpCoreCount;
+        }else{
+            maxCoreCount = tmpCoreCount-1;
+        }
+
+        for(int i=0;i<maxCoreCount;i++){
+            contentPane.add(new FireButton());
+        }
+
+        FireButton sizeTmp = new FireButton();
+        int fireButtonHeight = (int)Math.floor(sizeTmp.getPreferredSize().getHeight());
+        int fireButtonWidth = (int)Math.floor(sizeTmp.getPreferredSize().getWidth());
+
+        setSize(35+fireButtonWidth*this.maxCoreCount,45+fireButtonHeight);
+    }
 }
